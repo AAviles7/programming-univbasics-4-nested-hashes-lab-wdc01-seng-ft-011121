0950b4db656750f56f81ca90b37a4e004bf4ceab
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 09:10:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby",
-                                          :languages => ["LISP", "C"]}
- 
+  programmer_hash[:yukihiro_matsumoto] = Hash.new
+  programmer_hash[:yukihiro_matsumoto][] = :known_for => "Ruby", :languages => ["Lisp","C"] 
   return programmer_hash[:yukihiro_matsumoto]
 end
 
